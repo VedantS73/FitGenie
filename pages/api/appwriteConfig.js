a4bab1771d5f9324768c69c1ca31dbbb095c3b77
VedantS73 <vedantsawant72003@gmail.com>
2024-03-23 22:36:50 +0530

    stage

--- pages/api/appwriteConfig.js
+++ pages/api/appwriteConfig.js
@@ -1,10 +1,10 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, Databases, ID } from "appwrite";
 
 const client = new Client();
 
 const appwrite_api_key = '65fdbf643f997e6ea8f1';
 // const database_key = 'f7c948c8f7025238d93befa9906d1dde8028ba2e0e8fb44c87a50e531544da482b4d9469dbb5b7518dc98fd20a79698d9e70e05411af14c7c19e12273ea64f83f797f694a5391e4ae7c3cabf6663b63f1482d582ea81434900b171fad04c8e5d86948c460dc7a9d81c1dddf5433d8ef0a4494a4bb111e48d9ae0ac509d9419f8';
-const database_key = '65fead4a0d50fe942440';
+const database_id = '65fead4a0d50fe942440';
 
 // Set the endpoint URL and project ID for the client
 client
@@ -15,4 +15,17 @@ client
 export const account = new Account(client);
 
 // Create a new databases instance using the client and database ID
-export const databases = new Databases(client, database_key);
+export const databases = new Databases(client);
+
+export const createPlan = (user_id,planstring) => databases.createDocument(
+  database_id,
+  '65feae246cd725f25e0d', //connection id for : My Plans
+  ID.unique(),
+  { "user": user_id, "planstring": planstring }
+);
+
+// createContainer.then(function (response) {
+//   console.log(response);
+// }, function (error) {
+//   console.log(error);
+// });
